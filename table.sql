3e51ae5eb3a4700d659edfa7fd3a1928fc576fe1
yuvalpress <47568615+yuvalpress@users.noreply.github.com>
2021-03-23 14:09:01 +0200

    Delete table.sql

--- table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `users_dateTime` (                                                                                                                        `user_id` int(11) NOT NULL,
-`user_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-`creation_date` datetime NOT NULL,
-PRIMARY KEY (`user_id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
